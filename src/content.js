4b94ba267257ecee2e15b9cb0d36f2a7d9a27003
Takerou <harehare753@gmail.com>
2019-10-12 14:41:08 +0900

    add comment about this script
    参考URLの追加

--- src/content.js
+++ src/content.js
@@ -1,5 +1,11 @@
 function openwindow(){
     var win = window.open('https://www.youtube.com/watch?v=zZ4yQvxbOEA','_blank');
     //window.open('開きたい好きなURL','_blank');
+    
+    //ネタ元
+    //https://chrome.google.com/webstore/detail/just-do-it/ajgkkngocchijflmkhdgcdcabedenjah?hl=ja&gl=JP
+
+    //参考
+    //https://qiita.com/RyBB/items/32b2a7b879f21b3edefc
 }
 openwindow();
